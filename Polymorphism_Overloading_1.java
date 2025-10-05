2a69d7e3def358ee932b06739dc143e3edd4049a
Logesh <logeshspltm@gmail.com>
2025-10-05 10:30:59 +0530

    Overloading_1

--- Polymorphism_Overloading_1.java
+++ Polymorphism_Overloading_1.java
@@ -18,7 +18,7 @@ public class Main
 {
 	public static void main(String[] args) {
 		Calculator c1 = new Calculator();
-		System.out.println(c1.add(5,9));
-		System.out.println(c1.add(5,9));
+		System.out.println(c1.add(5,9)); // THIS WILL CALL THE FIRST METHOD
+		System.out.println(c1.add(5.5,9.1)); // THIS WILL CALL THE DIVISION METHOD
 	}
 }
